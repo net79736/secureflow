7cb92fe6069e1244276286920473472f07f7b8bf
net79736 <34103253+net79736@users.noreply.github.com>
2025-02-08 03:56:27 +0900

    initial-commit

---
+++ src/main/java/com/tdd/secureflow/SecureflowApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.secureflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tdd/secureflow/SecureflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.secureflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
